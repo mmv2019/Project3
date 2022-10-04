51c1a5102cedf0e13d797a189b3afff3a38cf5e5
Maryam Maged Victor <56774613+mmv2019@users.noreply.github.com>
2022-10-04 22:25:20 +0200

    mmmmmm

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://project3-env.eba-efpsa2ii.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://project3-env.eba-efpsa2ii.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
